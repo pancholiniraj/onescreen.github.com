dc1d0d6354c80ca82964346e165ccce789657785
pancholiniraj <pancholi_niraj@yahoo.ca>
2013-06-05 17:11:29 -0600

    Create Answer 1 - FizzBuzz.rb
    Interview Assignment - Niraj Pancholi

---
+++ Answer 1 - FizzBuzz.rb
@@ -0,0 +1,19 @@
+#Author: Niraj Pancholi
+
+for i in 1..100
+
+  if i % 3 == 0			
+		print "Fizz"
+	end	
+   
+	if i % 5 == 0
+		print "Buzz"
+	end
+	
+	if i % 3 != 0 and i % 5 != 0
+   		print i
+	end
+
+	puts""
+	
+end
