0950ea9d7ea685cb1d87f9f25305130ce2bb13b4
pancholiniraj <pancholi_niraj@yahoo.ca>
2013-06-05 17:13:29 -0600

    Create Answer 4 - FibonacciNonRecursionFunction.rb
    Interview Assignment - Niraj Pancholi

---
+++ Answer 4 - FibonacciNonRecursionFunction.rb
@@ -0,0 +1,29 @@
+#Author: Niraj Pancholi
+
+def FibonacciNonRecursionFunction(n=0)  
+  
+	a = 0
+	b = 1
+	k = 0
+
+  while n > 0
+		k = a + b
+		b = a
+		a = k
+		n = n - 1
+	end
+	
+	return k
+
+end
+
+
+ 
+puts
+print "Enter a number: "
+n = gets.chomp
+puts
+
+var = FibonacciNonRecursionFunction(Integer(n))
+print "Fibonacci: f(", n, ") = ", var
+puts
